3319a1b65898b458b9d8ddb03f734f9b1bfe8212
Fabian Zarate <zaratefabian472@gmail.com>
2023-06-14 20:44:51 -0300

    Initial Commit

---
+++ RickyAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickyAndMorty
+//
+//  Created by fabian zarate on 14/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
