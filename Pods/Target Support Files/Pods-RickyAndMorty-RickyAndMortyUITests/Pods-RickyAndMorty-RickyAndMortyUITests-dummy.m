0c864021bee67b169fabc988c9821a7a3b9fdc67
lanzarlosdados <zaratefabian@hotmail.com>
2023-07-17 14:54:20 -0300

    Install cocapods and kingfisher

---
+++ Pods/Target Support Files/Pods-RickyAndMorty-RickyAndMortyUITests/Pods-RickyAndMorty-RickyAndMortyUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RickyAndMorty_RickyAndMortyUITests : NSObject
+@end
+@implementation PodsDummy_Pods_RickyAndMorty_RickyAndMortyUITests
+@end
