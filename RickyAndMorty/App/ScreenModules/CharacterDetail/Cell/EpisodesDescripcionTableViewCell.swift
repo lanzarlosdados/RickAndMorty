97965aa6e7549c68c2c08d2222e3f03a14492e62
lanzarlosdados <zaratefabian@hotmail.com>
2023-07-18 21:08:32 -0300

    create cell episode

--- RickyAndMorty/App/ScreenModules/CharacterDetail/Cell/EpisodesDescripcionTableViewCell.swift
+++ RickyAndMorty/App/ScreenModules/CharacterDetail/Cell/EpisodesDescripcionTableViewCell.swift
@@ -9,15 +9,14 @@ import UIKit
 
 class EpisodesDescripcionTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var episode: UILabel!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var airDate: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
     
 }
